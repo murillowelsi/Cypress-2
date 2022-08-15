072ab811724c492c7c0f6e41fbb947b6b25ee3a6
Sam <iam@samuelavinash.com>
2022-08-15 19:30:04 +1000

    test

--- cypress/e2e/endToend/readPdf.js
+++ cypress/e2e/endToend/readPdf.js
@@ -1,11 +1,11 @@
-
+//work in progress
 it('downloads a simple PDF file', () => {
 
     cy.visit('http://www.africau.edu/')
   
     cy.contains('Download')
       .click()
-  
+    cy.log('this is pdf read test')
     cy.readFile('cypress/downloads/simple.pdf', 'utf8')
     cy.task('readPdf', 'cypress/downloads/simple.pdf')
   
