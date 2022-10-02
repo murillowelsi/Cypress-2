7580b248dfefaacdb05891ca3250f5b69ea9f666
Murillo Welsi <murillo.welsi@gmail.com>
2022-10-02 15:21:27 +0100

    fixing infinity loop

--- cypress/e2e/playground/play.cy.js
+++ cypress/e2e/playground/play.cy.js
@@ -1,65 +1,13 @@
- describe('Some Tests', () => {
-  before(() => {
-      // runs once before all tests
-    cy.generateFixture()
-  })
-  
-const testdata = require('../../fixtures/play.json')
+import { generateFixture } from "../../fixtures/play";
 
-  testdata.hits.forEach((data) => {
-    it(`${data.objectId}`, () => {
-        cy.log(JSON.stringify(data))
-    })
-  }) 
-})   
+describe("Creates each item", () => {
+  const testData = generateFixture()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // describe('Hacker Stories', () => {
-  //   before(() => {
-  //     // runs once before all tests
-  //     cy.generateFixture()
-  //   })
-    
-  //   let testdata = '../../fixtures/play.json'
-  //   testdata.hits.forEach((data) => {
-  //   it(`${data.objectID}`, () => {
-  //       cy.log(JSON.stringify(data))
-  //   })
-  // })
-
-/* WORKS but if one of the object fails while sending to the api it wont probaly try the rest of them */
-
-// it('Tests', () => {
-//     cy
-//     .fixture('play')
-//     .then((list) => {
-//       // cy.log(JSON.stringify(list.hits))
-//       list.hits.forEach((item) => {
-//         cy.log(JSON.stringify(item))
-//         })
-//     })
-// })
+  context(`Test Context`, () => {
+    testData.hits.forEach((hit) => {
+      it(`Creates item with id: ${hit.objectId}`, () => {
+        cy.log(JSON.stringify(hit));
+      });
+    });
+  });
+});
---
+++ cypress/fixtures/play.js
@@ -0,0 +1,229 @@
+import { faker } from "@faker-js/faker";
+
+export const generateFixture = () => {
+  return {
+    hits: [
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      {
+        objectId: `${faker.datatype.uuid()}`,
+        exists: `${faker.datatype.boolean()}`,
+        message: {
+          title: `${faker.lorem.words()}`,
+          url: `${faker.internet.url()}`,
+          author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          num_comments: `${faker.datatype.number()}`,
+          points: `${faker.datatype.number()}`,
+        },
+      },
+      
+    ],
+  };
+};
